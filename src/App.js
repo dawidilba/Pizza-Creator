dda42931376a9fb30292a0ce416319bcde709146
dawidilba <dawidilba@gmail.com>
2020-08-29 23:48:10 +0200

    Project logic - costs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Order from './components/Order';
 import Create from './components/Create';
 import Header from './components/Header'
 import './App.css';
 
 export default function App(props){
+    const [order, setOrder] = useState();
+    const getToOrder = (el) => {
+        setOrder(el)
+    }
     return (
         <>
             <Header />
             <main>
-                <Create />
+                <Create getPizza = {getToOrder}/>
                 <Order />
             </main>
         </>
--- src/components/Create.js
+++ src/components/Create.js
@@ -3,29 +3,74 @@ import Ingredients from '../ingredients.json'
 
 export default function Create(props){
     const [ingredients, setIngredients] = useState([]);
+    const [cost, setCost] = useState(0);
+    const [pizza, setPizza] = useState(0);
     useEffect(()=>{
+        Ingredients.map(el => el.checked = false);
         setIngredients(Ingredients);
     }, [])
+    useEffect(() => {
+        let result = pizza;
+        ingredients.map(ingredient => {
+            if(ingredient.checked === true){
+                result += ingredient.price;
+            }
+        })
+        setCost(result);
+    }, [ingredients, pizza])
+    const ingredientsCheck = (el) => {
+        setIngredients(
+        ingredients.map(ingredient => {
+            if(ingredient.name === el.name){
+                ingredient.checked = !ingredient.checked;
+            }
+            return ingredient;
+        }));
+    };
+    const pizzaInfo = () => {
+        let size = "";
+        switch(pizza){
+            case 11: 
+            size = "small";
+            break;
+            case 14:
+            size = "medium";
+            break;
+            case 17:
+            size = "big";
+        }
+        let ingredientsArr = [];
+        ingredients.forEach((el) => {
+            if(el.checked === true){
+                ingredientsArr.push(el.name);
+            }
+        })
+        if(size != ""){
+            props.getPizza({size : size, cost: cost, ingredients : ingredientsArr });
+        }
+    }
     return (
         <div>
             <h2>Choose your favourite ingredients!</h2>
+            <h3> Cost : {cost.toFixed(2)}zł</h3>
+            <button onClick={() => pizzaInfo()}>Order</button>
             <div class="pizzaContainer">
-                <img class="pizza small"src={process.env.PUBLIC_URL + "assets/pizzaSize.jpg"} alt="small pizza"/>
-                <span class="mark">&#10003;</span>
+                <img className={"pizza small" + (pizza === 11 ? " opacity" : "")} src={process.env.PUBLIC_URL + "assets/pizzaSize.jpg"} alt="small pizza" onClick={() => setPizza(11)}/>
+                <span className={ pizza===11 ? "mark" : "displayNone"}>&#10003;</span>
             </div>
             <div class="pizzaContainer">
-                <img class="pizza medium"src={process.env.PUBLIC_URL + "assets/pizzaSize.jpg"} alt="medium pizza"/>
-                <span class="mark">&#10003;</span>
+                <img className={"pizza medium" + (pizza === 14 ? " opacity" : "")} src={process.env.PUBLIC_URL + "assets/pizzaSize.jpg"} alt="medium pizza" onClick={() => setPizza(14)}/>
+                <span className={ pizza===14 ? "mark" : "displayNone"}>&#10003;</span>
             </div>
             <div class="pizzaContainer">
-                <img class="pizza big"src={process.env.PUBLIC_URL + "assets/pizzaSize.jpg"} alt="big pizza"/>
-                <span class="mark">&#10003;</span>
+                <img className={"pizza big" + (pizza === 17 ? " opacity" : "")} src={process.env.PUBLIC_URL + "assets/pizzaSize.jpg"} alt="big pizza" onClick={() => setPizza(17)}/>
+                <span className={ pizza===17 ? "mark" : "displayNone"}>&#10003;</span>
             </div>
             <div className="ingredientContainer">  
-            { Ingredients.map((el, index)=> {
+            { ingredients.map((el, index)=> {
                 return (
                     <div className="ingredient" key={index}>
-                        <input type="checkbox"></input>
+                        <input type="checkbox" checked={el.checked} onChange={()=> ingredientsCheck(el)}/>
                         <img className="ingredientIcon" src={process.env.PUBLIC_URL+"/assets/"+el.name+".png"} alt={el.name}/>
                         <p>{el.name}</p>
                         <p>{el.price.toFixed(2)}zł</p>
