b09ecff40d139cbb68c5697ac0c7e471865967e6
dawidilba <dawidilba@gmail.com>
2020-09-03 23:23:42 +0200

    Updates

---
+++ precache-manifest.73abc082d0342935b7872aba6e6bcff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06615d9641b8538a54fe9f828c9c5ca7",
+    "url": "/Pizza-Creator/index.html"
+  },
+  {
+    "revision": "9ba9a78339065d5f551a",
+    "url": "/Pizza-Creator/static/css/main.d76e708e.chunk.css"
+  },
+  {
+    "revision": "03ab4268630cd15a4266",
+    "url": "/Pizza-Creator/static/js/2.b4476a76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pizza-Creator/static/js/2.b4476a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba9a78339065d5f551a",
+    "url": "/Pizza-Creator/static/js/main.d7712c71.chunk.js"
+  },
+  {
+    "revision": "01e7ef274102fd031791",
+    "url": "/Pizza-Creator/static/js/runtime-main.ba7f81be.js"
+  }
+]);
